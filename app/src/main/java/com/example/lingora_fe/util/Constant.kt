17c44237642aee3efab7f957fad7ffa7ed4a6722
troller290 <23520867@gm.uit.edu.vn>
2026-01-03 11:57:49 +0700

    fix UI 1 tý

--- app/src/main/java/com/example/lingora_fe/util/Constant.kt
+++ app/src/main/java/com/example/lingora_fe/util/Constant.kt
@@ -1,6 +1,6 @@
 package com.example.lingora_fe.util
 
 object Constant {
-    const val BASE_URL = "https://lingora-be-dxce.onrender.com"
+    const val BASE_URL = "https://9169cc3901e5.ngrok-free.app"
     const val ENABLE_SOCKET_NOTIFICATIONS = true
 }
